5b4c1b50d5cf2bc40385a71c02d1700897a711bc
Merhawi <merhawitsegay007@gmail.com>
2024-03-19 01:09:57 -0700

    modify UI

--- app.py
+++ app.py
@@ -47,8 +47,16 @@ QA = RetrievalQA.from_chain_type(
 
 @app.route("/")
 def index():
-    return render_template('chat.html')
+    return render_template('chat_2.html')
 
+@app.route("/get", methods=["GET","POST"])
+def chat():
+    msg= request.form["msg"]
+    input = msg
+    print(input)
+    result = QA({"query" : input })
+    print("Response : ",result["result"])
+    return str(result["result"])
 
 if __name__== '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
